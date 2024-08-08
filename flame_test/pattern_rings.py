f55699365424ab9b80e62ca04a018983a090f49d
Pixelprizm <pixelprizm@gmail.com>
2024-08-07 20:16:23 -0700

    [utils] Create utils starting with ring indices

--- flame_test/pattern_rings.py
+++ flame_test/pattern_rings.py
@@ -4,12 +4,8 @@
 
 import flame_test as ft
 from time import sleep
+import utils as u
 
-lower_star = [0, 1, 2, 3, 4]
-lower_diagonal = [5, 6, 7, 8, 9]
-middle_ring = [10, 11, 12, 13, 14, 15, 16, 17, 18, 19]
-upper_diagonal = [20, 21, 22, 23, 24]
-upper_star = [25, 26, 27, 28, 29]
 sleep_between = 0.15
 
 def pattern_rings(state: ft.LightCurveState) -> bool:
@@ -20,36 +16,36 @@ def pattern_rings(state: ft.LightCurveState) -> bool:
     index = 0
 
     for reps in range(5):
-        for i in range(len(lower_star)):
-            state.s.solenoids[lower_star[i]] = 1
+        for i in range(len(u.lower_star)):
+            state.s.solenoids[u.lower_star[i]] = 1
         sleep(sleep_between)
 
-        for i in range(len(lower_star)):
-            state.s.solenoids[lower_star[i]] = 0
-        for i in range(len(lower_diagonal)):
-            state.s.solenoids[lower_diagonal[i]] = 1
+        for i in range(len(u.lower_star)):
+            state.s.solenoids[u.lower_star[i]] = 0
+        for i in range(len(u.lower_diagonal)):
+            state.s.solenoids[u.lower_diagonal[i]] = 1
         sleep(sleep_between)
 
-        for i in range(len(lower_diagonal)):
-            state.s.solenoids[lower_diagonal[i]] = 0
-        for i in range(len(middle_ring)):
-            state.s.solenoids[middle_ring[i]] = 1
+        for i in range(len(u.lower_diagonal)):
+            state.s.solenoids[u.lower_diagonal[i]] = 0
+        for i in range(len(u.middle_ring)):
+            state.s.solenoids[u.middle_ring[i]] = 1
         sleep(sleep_between)
 
-        for i in range(len(middle_ring)):
-            state.s.solenoids[middle_ring[i]] = 0
-        for i in range(len(upper_diagonal)):
-            state.s.solenoids[upper_diagonal[i]] = 1
+        for i in range(len(u.middle_ring)):
+            state.s.solenoids[u.middle_ring[i]] = 0
+        for i in range(len(u.upper_diagonal)):
+            state.s.solenoids[u.upper_diagonal[i]] = 1
         sleep(sleep_between)
 
-        for i in range(len(upper_diagonal)):
-            state.s.solenoids[upper_diagonal[i]] = 0
-        for i in range(len(upper_star)):
-            state.s.solenoids[upper_star[i]] = 1
+        for i in range(len(u.upper_diagonal)):
+            state.s.solenoids[u.upper_diagonal[i]] = 0
+        for i in range(len(u.upper_star)):
+            state.s.solenoids[u.upper_star[i]] = 1
         sleep(sleep_between)
 
-        for i in range(len(upper_star)):
-            state.s.solenoids[upper_star[i]] = 0
+        for i in range(len(u.upper_star)):
+            state.s.solenoids[u.upper_star[i]] = 0
 
     state.fill_solenoids(0)
     sleep(0.3)
