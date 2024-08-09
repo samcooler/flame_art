dd0afb58f1a1e15c2acb62c2fba8b13f0257ec96
Pixelprizm <pixelprizm@gmail.com>
2024-08-08 19:08:01 -0700

    [utils] triples array and test pattern for it

---
+++ flame_test/pattern_test_triples.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3
+
+# Author: Eric Harper-Gauderman
+
+import flame_test as ft
+from time import sleep
+import utils as u
+
+sleep_between = 0.3
+
+def pattern_test_triples(state: ft.LightCurveState) -> bool:
+    # Start
+    state.fill_solenoids(0)
+    state.fill_apertures(1.0)
+
+    # Pattern
+    for triple in u.triples:
+        for nozzle in triple:
+            state.s.solenoids[nozzle] = 1
+
+        sleep(sleep_between)
+
+        for nozzle in triple:
+            state.s.solenoids[nozzle] = 0
+
+        sleep(sleep_between)
+
+    # End
+    state.fill_solenoids(0)
+    sleep(0.3)
