d21fbbfcffa370bc51e531d123f62e71d8574a82
Pixelprizm <pixelprizm@gmail.com>
2024-08-08 19:08:01 -0700

    [utils] opposite and test pattern for it

---
+++ flame_test/pattern_test_opposite.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+
+# Author: Eric Harper-Gauderman
+
+import flame_test as ft
+from time import sleep
+import utils as u
+
+sleep_between = 0.3
+
+def pattern_test_opposite(state: ft.LightCurveState) -> bool:
+    # Start
+    state.fill_solenoids(0)
+    state.fill_apertures(1.0)
+
+    # Pattern
+    for i in range(0, 30):
+        state.s.solenoids[i] = 1
+        state.s.solenoids[u.opposite[i]] = 1
+        sleep(sleep_between)
+        state.s.solenoids[i] = 0
+        state.s.solenoids[u.opposite[i]] = 0
+
+    # End
+    state.fill_solenoids(0)
+    sleep(0.3)
