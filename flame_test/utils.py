6cbc9e481741df1c6801ecb119e75511063deec3
Pixelprizm <pixelprizm@gmail.com>
2024-08-08 19:08:01 -0700

    [utils] stars array and test pattern for it

--- flame_test/utils.py
+++ flame_test/utils.py
@@ -1,11 +1,33 @@
 
 
+stars = [
+    # lower star
+    [0, 1, 2, 3, 4],
+
+    # lower diagonal stars
+    [0, 5, 10, 19, 9],
+    [1, 6, 12, 11, 5],
+    [2, 7, 14, 13, 6],
+    [3, 8, 16, 15, 7],
+    [4, 9, 18, 17, 8],
+
+    # upper diagonal stars
+    [20, 25, 21, 11, 10],
+    [26, 21, 12, 13, 22],
+    [27, 22, 14, 15, 23],
+    [28, 23, 16, 17, 24],
+    [29, 24, 18, 19, 20],
+
+    # upper star
+    [25, 26, 27, 28, 29],
+]
+
 ring_to_idx = {
-    'lower_star': [0, 1, 2, 3, 4],
+    'lower_star': stars[0],
     'lower_diagonal': [5, 6, 7, 8, 9],
     'middle_ring': [10, 11, 12, 13, 14, 15, 16, 17, 18, 19],
     'upper_diagonal': [20, 21, 22, 23, 24],
-    'upper_star': [25, 26, 27, 28, 29],
+    'upper_star': stars[11],
 }
 
 idx_to_ring = {}
