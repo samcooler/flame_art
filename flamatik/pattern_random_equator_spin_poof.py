5e5f559353a42e2a4ae69626be1d37b25a76b271
Pixelprizm <pixelprizm@gmail.com>
2024-08-10 12:41:22 -0700

    [patterns] pattern_random_star_spin_poof and pattern_random_equator_spin_poof

---
+++ flamatik/pattern_random_equator_spin_poof.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+
+# Author: Eric Harper-Gauderman
+
+import random
+import flamatik as ft
+from time import sleep
+import face_groupings as g
+
+def pattern_random_equator_spin_poof(state: ft.LightCurveState) -> bool:
+    # Start
+    state.fill_solenoids(0)
+    state.fill_apertures(1.0)
+
+    equator_index = random.randint(1, len(g.equators)) - 1
+
+    # Repeat 3 times cause it's cool
+    for _ in range(3):
+        # Poof selected equator one face at a time
+        for nozzle in g.equators[equator_index]:
+            state.s.solenoids[nozzle] = 1
+            sleep(0.2)
+            state.s.solenoids[nozzle] = 0
+
+    # Longer sleep at end since it looks better when repeated
+    sleep(0.6)
---
+++ flamatik/pattern_random_star_spin_poof.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+
+# Author: Eric Harper-Gauderman
+
+import random
+import flamatik as ft
+from time import sleep
+import face_groupings as g
+
+def pattern_random_star_spin_poof(state: ft.LightCurveState) -> bool:
+    # Start
+    state.fill_solenoids(0)
+    state.fill_apertures(1.0)
+
+    star_index = random.randint(0, len(g.stars) - 1)
+
+    # Poof selected star one face at a time
+    for nozzle in g.stars[star_index]:
+        state.s.solenoids[nozzle] = 1
+        sleep(0.2)
+        state.s.solenoids[nozzle] = 0
+
+    # Longer sleep at end since it looks better when repeated
+    sleep(0.6)
