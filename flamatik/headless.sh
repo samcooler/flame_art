8010505918504417fdbb392816be25d53cd76dbe
Brian Bulkowski <brian@bulkowski.org>
2024-08-20 12:58:23 -0700

    Update auto-start so it worksxz

--- flamatik/headless.sh
+++ flamatik/headless.sh
@@ -1,1 +1,14 @@
-python flamatik.py -c lightcurve.cnf -l playlist.json
+#!/bin/sh
+echo "starting flamatik service with home $HOME"
+# have to init pyenv
+export PYENV_ROOT="/home/pi/.pyenv"
+export PATH="$PYENV_ROOT/bin:$PATH"
+eval "$(pyenv init --path)"
+eval "$(pyenv init -)"
+# pyenv global 3.12.5
+
+python --version
+
+python /home/pi/flame_art/flamatik/flamatik.py -c lightcurve.cnf -l playlist.json
+
+echo "exiting flamatik service"
