71026db285aeb21db8c88c60d6f061697e42ae21
Pixelprizm <pixelprizm@gmail.com>
2024-08-25 00:43:36 -0700

    [patterns] pattern_trail and fix pattern_trail_up

---
+++ flamatik/pattern_trail.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python3
+
+# Author: Eric Harper-Gauderman
+
+import flamatik as ft
+from time import sleep
+import numpy as np
+import face_groupings as g
+import geometry_math as m
+
+old_gravity = None
+
+# Opens several solenoids facing away from the direction of motion, based in imu.
+# The ones facing less directly up will have lower intensity.
+# You should repeat this a lot, 20*desired seconds. 200 for 10 seconds.
+def pattern_trail(state: ft.LightCurveState) -> bool:
+    global old_gravity
+    state.fill_solenoids(0)
+    state.fill_apertures(0.0)
+
+    if old_gravity == None:
+        old_gravity = (state.s.gravity[0], state.s.gravity[1], state.s.gravity[2])
+    elif old_gravity[0] != state.s.gravity[0] and old_gravity[1] != state.s.gravity[1] and old_gravity[2] != state.s.gravity[2]:
+        motion_direction = (state.s.gravity[0] - old_gravity[0], state.s.gravity[1] - old_gravity[1], state.s.gravity[2] - old_gravity[2])
+        old_gravity = (state.s.gravity[0], state.s.gravity[1], state.s.gravity[2])
+
+        motion_direction /= np.linalg.norm(motion_direction)
+
+        for nozzle in g.all_nozzles:
+            d = m.dot(m.nozzle_vectors[nozzle], motion_direction)
+            if d > 0.0:
+                state.s.solenoids[nozzle] = 1
+                state.s.apertures[nozzle] = min(d, 1.0)
+
+    sleep(0.05)
--- flamatik/pattern_trail_up.py
+++ flamatik/pattern_trail_up.py
@@ -4,6 +4,7 @@
 
 import flamatik as ft
 from time import sleep
+import numpy as np
 import face_groupings as g
 import geometry_math as m
 
@@ -13,10 +14,13 @@ def pattern_trail_up(state: ft.LightCurveState) -> bool:
     state.fill_solenoids(0)
     state.fill_apertures(0.0)
 
-    reverse_gravity = (-state.s.gravity[0], -state.s.gravity[1], -state.s.gravity[2])
+    reverse_gravity = np.array([-state.s.gravity[0], -state.s.gravity[1], -state.s.gravity[2]])
+    reverse_gravity /= np.linalg.norm(reverse_gravity)
     for nozzle in g.all_nozzles:
-        d = m.dot(m.nozzle_vectors[nozzle], reverse_gravity)
+        d = m.dot(m.nozzle_vectors[nozzle], (reverse_gravity[0], reverse_gravity[1], reverse_gravity[2]))
         if d > 0.0:
             state.s.solenoids[nozzle] = 1
-            state.s.apertures[nozzle] = d
+            print(d)
+            state.s.apertures[nozzle] = min(d, 1.0)
     sleep(0.05)
+    
